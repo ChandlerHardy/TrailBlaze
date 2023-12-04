889986abff2adc7c3c2729827184b9ed3cbad3da
chandler-0411 <hardych04@gmail.com>
2023-12-03 23:25:44 -0600

    Added review form to show page and added route to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const Review = require("./models/review.js");
 
 main().catch((err) => console.log(err));
 
@@ -101,6 +102,19 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+// Review Route
+app.post(
+  "/campgrounds/:id/reviews",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
