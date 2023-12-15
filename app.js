94a8edfa2cb5ae3987c97e5b6e97eaa2f4fa8ce8
chandler-0411 <hardych04@gmail.com>
2023-12-15 11:13:04 -0600

    Added and configured express session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
@@ -27,6 +28,18 @@ app.use(methodOverride("_method"));
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "this is my secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 app.get("/", (req, res) => {
   res.render("home");
